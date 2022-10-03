fbb2a10396a2bf7a2077c4625e7979e7005a967c
fatihmaras <fatihmmaras@gmail.com>
2022-10-04 02:58:36 +0300

    Student system

---
+++ EB-P6_StudentSystem/Ogrenci.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EB_P6_StudentSystem
+{
+    internal class Ogrenci
+    {
+      
+        
+            private int ogrno;
+            private string isim;
+            private string soyisim;
+            private int vize1;
+            private int vize2;
+            private int final;
+            private string OkulIsmi;
+
+
+
+            public Ogrenci(int Xogrno, string Xisim, string Xsoyisim, int Xvize1, int Xvize2, int Xfinal, string XokulIsmi)
+            {
+                ogrno = Xogrno;
+                isim = Xisim;
+                soyisim = Xsoyisim;
+                vize1 = Xvize1;
+                vize2 = Xvize2;
+                final = Xfinal;
+                OkulIsmi = XokulIsmi;
+
+            }
+
+            public void ogrenciBilgileriGoster()
+            {
+                Console.WriteLine("Öğrenci Numarası : " + ogrno);
+                Console.WriteLine("Öğrenci İsmi : " + isim);
+                Console.WriteLine("Öğrenci Soyismi: " + soyisim);
+                Console.WriteLine("Vize 1: " + vize1);
+                Console.WriteLine("Vize 2: " + vize2);
+                Console.WriteLine("Final : " + final);
+                Console.WriteLine("Okul İsmi : " + OkulIsmi);
+
+
+            }
+
+            public double OgrenciNotubul()
+            {
+                double ortalama = vize1 * 0.2 + vize2 * 0.2 + final * 0.6;
+                return ortalama;
+
+            }
+            public void Okulgetir()
+            {
+                Console.WriteLine("Öğrencinin Okul İsmi : " + OkulIsmi);
+            }
+
+        }
+    }
+
+
+
+
---
+++ EB-P6_StudentSystem/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_OgrenciUygulamasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Kontrol = true;
+
+            Ogrenci ogrenci1 = new Ogrenci(1, "Fatih", "Maraş", 50, 60, 70, "İTÜ");
+
+            Console.WriteLine("Uygulamamıza Hoşgeldiniz... Yapmak İstediğiniz İşlemi Seçiniz ");
+
+
+            while (Kontrol)
+            {
+                IslemleriGoster();
+                string secim = Console.ReadLine();
+
+                switch (secim)
+                {
+
+                    case "1":
+                        ogrenci1.ogrenciBilgileriGoster();
+                        break;
+
+
+                    case "2":
+                        double OgrenciOrtalama = ogrenci1.OgrenciNotubul();
+                        Console.WriteLine("Öğrencinin Ortalaması : " + OgrenciOrtalama);
+                        break;
+
+                    case "3":
+                        ogrenci1.Okulgetir();
+
+                        break;
+
+
+
+                    case "4":
+                        Kontrol = false;
+                        break;
+
+
+                }
+
+
+
+            }
+
+        }
+        static void IslemleriGoster()
+        {
+            Console.WriteLine("1-Öğrenci Bilgilierini Göster");
+            Console.WriteLine("2- Öğrenci Ortalamasını Göster");
+            Console.WriteLine("3-Öğrenci Okulunu Göster");
+            Console.WriteLine("4-Çıkış Yap");
+        }
+    }
+}
